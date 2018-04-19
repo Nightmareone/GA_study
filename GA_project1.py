92d449b4efe0f3e1ddce651491551f7a98c8a86c
XU <mengyan9321@gmail.com>
2018-04-19 23:23:55 +0900

    work of 20180419

--- GA_project1.py
+++ GA_project1.py
@@ -1,1 +1,20 @@
 import numpy as np
+
+DNA_SIZE = 6            # DNA length
+PACKAGE_SIZE = 1000           # population size
+CROSS_RATE = 0.8         # mating probability (DNA crossover)
+MUTATION_RATE = 0.03    # mutation probability
+N_GENERATIONS = 200
+
+class GA(object):
+    def get_fitness():
+        pass
+
+    def select():
+        pass
+
+    def crossover():
+        pass
+
+    def mutate():
+        pass
